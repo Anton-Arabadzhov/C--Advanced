4b9062befd02e79117a89de0ed41b9e8e4f20896
Anton <anton.arabadzhov@gmail.com>
2020-12-22 09:14:36 +0000

    Advance Prepare

---
+++ FirstProject/FirstTestProject/Controllers/PhoneBookController.cs
@@ -0,0 +1,31 @@
+﻿using FirstTestProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstTestProject.Controllers
+{
+    public class PhoneBookController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(DataContext.Contacts);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Contact model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                DataContext.Contacts.Add(model);
+            }
+
+            return RedirectToAction(nameof(Index));
+            
+            
+        }
+    }
+}
---
+++ FirstProject/FirstTestProject/Models/Contact.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstTestProject.Models
+{
+    public class Contact
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Number { get; set; }
+    }
+}
