b7169e4a8ecca0e5bfa0bd41305c6b1665887e05
Anton <anton.arabadzhov@gmail.com>
2020-12-23 09:47:39 +0000

    more exercise

---
+++ Stack-Int/PrintEvenNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> odds = new Queue<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    odds.Enqueue(nums[i]);
+                }
+            }
+            while (odds.Count > 1)
+            {
+                Console.Write($"{odds.Dequeue()}, ");
+            }
+            Console.Write(odds.Peek());
+        }
+    }
+}
---
+++ Stack-Int/Supermarket/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            Queue<string> costumers = new Queue<string>();
+            int count = 0;
+            while (name != "End")
+            {
+                if (name == "Paid")
+                {
+                    while (costumers.Count > 0)
+                    {
+                        Console.WriteLine(costumers.Dequeue());
+                        count--;
+                    }
+                }
+                else
+                {
+                    costumers.Enqueue(name);
+                    count++;
+                }
+                name = Console.ReadLine();
+            }
+            Console.WriteLine($"{count} people remaining.");
+        }
+    }
+}
