ebac3b0c45406a4e52a1d560ab15d7bd831e300b
Anton <anton.arabadzhov@gmail.com>
2020-12-27 23:04:01 +0000

    more Exercise

---
+++ FunctionPrograming/CountUpperCaseWord/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CountUpperCaseWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
