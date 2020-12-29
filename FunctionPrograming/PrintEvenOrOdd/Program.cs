f7316d1c9d4405c8d6a336e18ae814fcf1071f61
Anton <anton.arabadzhov@gmail.com>
2020-12-29 18:01:09 +0000

    Exercise

---
+++ FunctionPrograming/PrintEvenOrOdd/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrintEvenOrOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int start = nums[0];
+            int end = nums[1];
+            string criteria = Console.ReadLine();
+
+            Func<int, int, List<int>> generateList = (start, end) =>
+            {
+                List<int> numbers = new List<int>();
+
+                for (int i = start; i < end; i++)
+                {
+                    numbers.Add(i);
+                }
+                return numbers;
+            };
+            List<int> numbers = generateList(start, end);
+            Func<int, bool> evenPredicate
+            numbers = numbers.Where();
+
+            //Predicate<int> evenPredicate = n => n % 2 == 0;
+            
+        }
+    }
+}
---
+++ MidExam/SantaPresent/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SantaPresent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            var input2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> matirialForCrafting = new Stack<int>(input1);
+            Queue<int> magicLevelValue = new Queue<int>(input2);
+
+            while (matirialForCrafting.Count > 0 && magicLevelValue.Count > 0)
+            {
+
+            }
+        }
+    }
+}
