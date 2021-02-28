02bd1021c71cb2bc5fdb1557b23f3d126ad5d954
Anton <anton.arabadzhov@gmail.com>
2021-02-28 06:20:56 +0000

    Exercise

---
+++ Ecapsulation/SortPersonsbyNameandAge/Person.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstname;
+        private string lastname;
+        private int age;
+        private decimal salary;
+
+
+
+
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+        public string FirstName
+        {
+            get
+            {
+                return this.firstname;
+            }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+                firstname = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return this.lastname;
+            }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+                lastname = value;
+            }
+        }
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+                age = value;
+            }
+        }
+        public decimal Salary
+        {
+            get
+            {
+                return this.salary;
+            }
+            private set
+            {
+                if (value < 460)
+                {
+                    throw new ArgumentException("Salary cannot be less than 460 leva!");
+                }
+                salary = value;
+            }
+        }
+        public void IncreaseSalary(decimal percentige)
+        {
+            if (Age < 30)
+            {
+                percentige /= 2;
+            }
+            Salary = Salary + Salary * percentige / 100;
+        }
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} receives {this.Salary:f2} leva.";
+        }
+    }
+}
